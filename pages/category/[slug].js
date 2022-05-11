56414fb5f62a3d9d326e4da0c0990446eac988d7
NezaBruce <nezabruce@gmail.com>
2022-05-11 14:05:34 +0200

    bugging

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -32,14 +32,10 @@ const Categories = ({posts}) => {
 export default Categories;
 export async function getStaticProps({ params }) {
   const posts = await getCategoryPost(params.slug);
-
   return {
     props: { posts },
   };
 }
-
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const categories = await getCategories();
   return {
@@ -47,3 +43,5 @@ export async function getStaticPaths() {
     fallback: true,
   };
 }
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useRouter} from 'next/router';
 import PostWidget from '../../components/PostWidget';
-import { Categories } from '../../components';
+// import { Categories } from '../../components';
 import { getPosts, getPostsDetails } from '../../services';
 import CommentsForm from '../../components/CommentsForm';
 import Loader from '../../components/Loader';
@@ -29,7 +29,7 @@ const PostDetails = ({post}) => {
         <div className="col-span-1 lg:col-span-4">
            <div className="relative lg:sticky top-8">
             <PostWidget slug={post.slug} categories={post.categories.map((category)=>category.slug)}/>
-            <Categories/>
+            {/* <Categories/> */}
            </div>
         </div>
       </div>
