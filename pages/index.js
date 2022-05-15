ec2be9276dafd08f5d4dd455a7a551f91c446f29
NezaBruce <nezabruce@gmail.com>
2022-05-15 21:59:49 +0200

    refactor

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ export default function Home({posts}) {
    <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-            ))}
+          {posts.map((post, index) => {
+            console.log("post"+post.node.featuredImage.url);
+            return <PostCard key={index} post={post.node} />
+          }
+            )}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -33,10 +35,3 @@ return {
   props:{posts},
 };
 };
-export async function getStaticPaths(){
-  const posts=await getPosts();
-  return{
-      paths:posts.map(({node:{slug}})=>({params:{slug}})),
-      fallback:true
-  }
-  }
